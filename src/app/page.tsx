440237437bfb66bce92d239228ab6a468668fed4
YeasinWebDev <ar9485727@gmail.com>
2024-04-03 11:03:32 +0600

    InfiniteMovingCards add

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,16 @@
 import FeaturedSec from "@/components/FeaturedSec";
 import HeroSection from "@/components/HeroSection";
+import MusicSchoolTestimonials from "@/components/TestimonialCards";
 import WhyChooseUs from "@/components/WhyChooseUs";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
-        <HeroSection/>
-        <FeaturedSec/>
-        <WhyChooseUs/>
+      <HeroSection />
+      <FeaturedSec />
+      <WhyChooseUs />
+      <MusicSchoolTestimonials />
     </main>
 
   );
